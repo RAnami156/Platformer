fdf99642f3e707e5af32493abf525c3c8fea12ff
RAnami156 <romaandreev414@gmail.com>
2025-01-03 21:36:47 +0100

    shop 0.1

--- shop.gd
+++ shop.gd
@@ -1,5 +1,12 @@
 extends Area2D
+var player_in_area = false
+
+func _physics_process(delta: float) -> void:
+	if player_in_area and Input.is_action_just_pressed("e"):
+		get_tree().change_scene_to_file("res://shop_lev.tscn")
 
 func _on_body_entered(body: Node2D) -> void:
-	if Input.is_action_pressed("shop"):
-		get_tree().change_scene_to_file("res://shop_lev.tscn")
+	player_in_area = true
+
+func _on_body_exited(body: Node2D) -> void:
+	player_in_area = false
--- shop_lev.gd
+++ shop_lev.gd
@@ -3,3 +3,9 @@ extends Node2D
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://level.tscn")
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("close"):
+		get_tree().change_scene_to_file("res://level.tscn")
+	
+	
