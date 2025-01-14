cc851deb05b400c05da14b6cb36482904f40c751
ANDRYEYEV Roman (123) <romaandreev414@gmail.com>
2025-01-14 12:07:09 +0100

    cheast 0.2

--- node_2d.gd
+++ node_2d.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
 @onready var anim = $AnimatedSprite2D
-var body_in 
+var body_in = false
+var chest_open = false 
 
-func _ready() -> void:
-	pass 
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("e") and body_in == true:
+		if chest_open:
+			anim.play("close") 
+			chest_open = false
+		else:
+			anim.play("open")  
+			chest_open = true
+	elif not chest_open:
+		anim.play("close") 
 
-func _process(delta: float) -> void:
-	pass
- 
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	body_in = true  # Объект вошел в область
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	body_in = false  # Объект покинул область
